c59f2681844f952a571504d64a9e8e16aafad4ba
Satoshi Nagasawa <nagasawa@ham.works>
2021-10-06 21:46:55 +0900

    add: 各種lint関連

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  ignorePatterns: ['.eslintrc.js', '.prettierrc.js', 'next.config.js', './dist/**/*.js'],
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'react', 'import'],
+  settings: {
+    react: {
+      version: 'detect'
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+      typescript: {
+        config: 'tsconfig.json',
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'react/prop-types': ['off'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    '@typescript-eslint/no-misused-new': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 'args': 'after-used' }],
+    'no-console': 'error',
+    'jsx-a11y/anchor-is-valid': 'off',
+    'jsx-a11y/no-onchange': 'off',
+    'no-irregular-whitespace': 'off',
+  },
+}
