90a8082dbbf5d747962f5b6bac8024d49bcd6e3d
Satoshi Nagasawa <nagasawa@ham.works>
2021-10-21 20:30:28 +0900

    fix: test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('アプリが正常に立ち上がるか', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const expectText = screen.getByText(/The password you created will be copied by clicking./i);
+  expect(expectText).toBeInTheDocument();
 });
